2a3b1f96560844b2c2a5d6424c2d3a386d4da930
wu xia <xia.wu@intel.com>
2017-06-22 15:37:43 +0800

    media : Fix kernel 4.9 compile error
    Signed-off-by: Chen, Angus <angus.chen@intel.com>
    Signed-off-by: wu xia <xia.wu@intel.com>

--- drivers/media/media-device.c
+++ drivers/media/media-device.c
@@ -938,9 +938,9 @@ struct media_links_enum32 {
 	compat_uptr_t links; /* struct media_link_desc * */
 	__u32 reserved[4];
 };
-
 static long media_device_enum_links32(struct media_device *mdev,
-				      struct media_links_enum32 __user *ulinks)
+						struct file *filp,
+						struct media_links_enum32 __user *ulinks)
 {
 	struct media_links_enum links;
 	compat_uptr_t pads_ptr, links_ptr;
@@ -955,7 +955,7 @@ static long media_device_enum_links32(struct media_device *mdev,
 	links.pads = compat_ptr(pads_ptr);
 	links.links = compat_ptr(links_ptr);
 
-	return media_device_enum_links(mdev, &links);
+	return media_device_enum_links(mdev, filp, &links);
 }
 
 #define MEDIA_IOC_ENUM_LINKS32		_IOWR('|', 0x02, struct media_links_enum32)
@@ -970,6 +970,7 @@ static long media_device_compat_ioctl(struct file *filp, unsigned int cmd,
 	case MEDIA_IOC_ENUM_LINKS32:
 		mutex_lock(&dev->graph_mutex);
 		ret = media_device_enum_links32(dev,
+				filp,
 				(struct media_links_enum32 __user *)arg);
 		mutex_unlock(&dev->graph_mutex);
 		break;
